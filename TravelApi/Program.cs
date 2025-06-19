4f9348f2e82f0e55ca8fb309e0be1531e89ec00c
Joey Medina <joeymedina32@gmail.com>
2025-06-18 21:57:21 -0500

    second

--- TravelApi/Program.cs
+++ TravelApi/Program.cs
@@ -13,6 +13,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
